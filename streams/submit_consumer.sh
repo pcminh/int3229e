6766f7b1104757ca869c5316a2569f6bf6cad53e
pcminh <pcminh@users.noreply.github.com>
2023-05-27 06:58:33 +0700

    streaming jobs

---
+++ streams/submit_consumer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+spark-submit \
+    --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.4.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0,org.postgresql:postgresql:42.6.0 \
+    congestion_consumer.py
